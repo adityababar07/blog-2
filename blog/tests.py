19aff6123dd14cb0f8fccc8b70a2584f43543652
hacker07 <hacker07@localhost.localdomain>
2020-10-15 20:51:37 -0400

    added a new feature (edit,delete option)

--- blog/tests.py
+++ blog/tests.py
@@ -21,6 +21,9 @@ class BlogTest(TestCase):
         post = Post(title = "a title")
         self.assertEqual(str(post), post.title)
     
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/') 
+    
     def test_post_content(self):
         self.assertEqual(f"{self.post.title}", "test")
         self.assertEqual(f"{self.post.author}", "aditya")
@@ -39,3 +42,24 @@ class BlogTest(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "test")
         self.assertTemplateUsed(response, "post_detail.html")
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+            'title': 'new_title',
+            'author': self.user,
+            'body': 'this is a test',
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'new_title')
+        self.assertContains(response, 'this is a test')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'update_title',
+            'body': 'new content',
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.post(reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 302)
--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
+from .views import (BlogListView,
+                    BlogDetailView,
+                    BlogCreateView,
+                    BlogUpdateView,
+                    BlogDeleteView)
 
 urlpatterns = [
+    path('post/delete/<int:pk>', BlogDeleteView.as_view(), name='post_delete'),
     path('post/edit/<int:pk>', BlogUpdateView.as_view(), name='post_edit'),
     path('post/new', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
