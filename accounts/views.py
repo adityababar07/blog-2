127e860a6fac7162359b557ef79b4c85062f47a4
root <root@localhost.localdomain>
2021-01-20 18:28:20 +0530

    profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import CreateView, TemplateView
 from .forms import CustomUserCreationForm
 
 # Create your views here.
@@ -9,3 +10,6 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'profile.html'
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'heroku stack:set container -a '
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['chitchatchithere.herokuapp.com','localhost','127.0.0.1']
 
