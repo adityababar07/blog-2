594d9f5d87bdd626a64f9c70d4abc0f6587a36a0
Aditya Babar <adityababar715@gmail.com>
2021-06-11 02:56:08 +0000

    debug value added to vars

--- study_project/settings.py
+++ study_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'heroku stack:set container -a '
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'heroku stack:set container -a'
 
 ALLOWED_HOSTS = ['chitchatchithere.herokuapp.com','localhost','127.0.0.1']
 
@@ -97,7 +97,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
