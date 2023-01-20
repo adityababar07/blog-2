27d19bb288592cf7316cdb69bddf017594eb9e94
hacker07 <hacker07@localhost.localdomain>
2023-01-20 11:29:15 +0530

    initial changes

--- study_project/settings.py
+++ study_project/settings.py
@@ -170,7 +170,7 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = 'postmaster@sandbox33bb34a8fcfb45d89ea6c0751c7fb69a.mailgun.org'
 EMAIL_HOST = 'smtp.mailgun.org'
 EMAIL_HOST_USER = 'postmaster@sandbox33bb34a8fcfb45d89ea6c0751c7fb69a.mailgun.org'
-EMAIL_HOST_PASSWORD = 'b0a22ab1e1054cec620160558edb62fb-f7d687c0-8b75379a'
+EMAIL_HOST_PASSWORD = 'e0b048bffdd6b374509b81e2be8455a7-f7d687c0-a9fadd3f'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
