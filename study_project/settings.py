6870cacbe55745399787ad5f9e08a36409ed420c
hacker07 <hacker07@localhost.localdomain>
2023-01-20 17:46:07 +0530

    initial changes

--- study_project/settings.py
+++ study_project/settings.py
@@ -170,7 +170,7 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = 'todoapp40@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'todoapp40@gmail.com'
-EMAIL_HOST_PASSWORD = '];0_`.NusWr22f@.mXbc"~*/em`_?hUb.p(t(T+Aw'
+EMAIL_HOST_PASSWORD = 'vtpuxziyetujujhz'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
